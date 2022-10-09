84588725adfaaba4544627a09338aa6c5e62a96b
Venkat5452 <venkatsai.bandi2019@gmail.com>
2022-10-10 00:34:05 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="bg-dark text-white text-center">
-            <div className="wrapper mt-3">
+            <div className="wrapper mt-3 p-2">
                 <small>
                     <strong>© All Rights Reserved 2022</strong> Disclaimer | Private Policy | Social Media Policy | SiteMap
                 </small>
