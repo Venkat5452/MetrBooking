055b7a1a63f9bc5324de260f6bdfd9c1284b8334
Venkat5452 <venkatsai.bandi2019@gmail.com>
2022-11-14 21:29:49 +0530

    cost

--- src/components/Loggedin.js
+++ src/components/Loggedin.js
@@ -2,22 +2,30 @@ import React from 'react'
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../Context/UserAuthContext";
-import { useState } from 'react';
+import { useState} from 'react';
+import { collection ,getDocs} from 'firebase/firestore';
 import { Alert } from "react-bootstrap";
 import { toDataURL } from 'qrcode';
 import jsPDF from 'jspdf';
 import hydemetro from '../images/img2.png';
 import moment from 'moment/moment';
-import img2 from '../images/img2.png'
+import img2 from '../images/img2.png';
+import PhoneInput from 'react-phone-number-input';
+import { db } from '../Firebase';
+
 //import displayrazorpay from '../utils/paymentgateway';
 
-
 //import { Document, Page, pdfjs } from "react-pdf";
+
 function Loggedin() {
     const { logOut, user } = useUserAuth();
+    const [number,setNumber]=useState("");
     const [valTo, setValTo] = useState("");
     const [valFrom, setValFrom] = useState("");
     const [error, setError] = useState("");
+    const [Amt, setAmt] = useState("");
+    const [flagc,setflagc]=useState("");
+    const [sendflag,setsendflag]=useState(false);
     const navigate = useNavigate();
     const [pay,setpay]=useState(false);
     const [img1, setIMG]=useState("");
@@ -32,41 +40,88 @@ function Loggedin() {
         console.log(error.message);
       }
     };
+    const sendPDF=async(e)=>{
+      e.preventDefault();
+      setError("");
+      setsendflag(true);
+    }
+
+    const sendpdf2=async(e)=>{
+      e.preventDefault();
+      setError("");
+      if(number==="") {
+        setError("Invalid Input");
+      }
+      else {
+        var date1 = moment();
+        var currentDate = date1.format('DD/MM/YYYY');
+        //console.log(currentDate);
+         var doc=new jsPDF('landscape','px','a5','false');
+         doc.addImage(hydemetro,'PNG',10,10,70,70);
+         doc.setTextColor("Blue");
+         doc.setFontSize(24);
+         doc.text(145,20,"Hyderabad Metro Ticket");
+         doc.setFontSize(12);
+         doc.setTextColor("Black");
+         doc.text(120,45,"Station From");
+         doc.text(195,45,"--");
+         doc.text(210,45,Stations[valFrom - 1]);
+         doc.text(120,75,"Station To");
+         doc.text(195,75,"--");
+         doc.text(210,75,Stations[valTo - 1]);
+         doc.text(120,105,"Valid Till");
+         doc.text(195,105,"--");
+         doc.text(210,105,currentDate + " ,11:45 pm ");
+         doc.addImage(img1,'PNG',160,115,125,125);
+         doc.setTextColor("Green");
+         doc.setFontSize(25);
+         doc.text(165,275,"Happy Journey");
+         setsendflag(false);
+         console.log(doc);
+         setIMG("")
+         setNumber("");
+         setpay(false);
+         setValTo("");
+         setValFrom("");
+         navigate("/Loggedin");
+      }
+    }
     const handleSubmit=async(e)=>{
       e.preventDefault();
       setError("");
+      setAmt("");
       //setQr("");
       if(valFrom === valTo || valFrom==="" || valTo==="") {
         setError("Invalid Input");
       }
       else {
-        var options= {
-          key:"rzp_test_Rg7Lgz7NgVAUCu",
-          key_secret:"x0e5GSKDeyVrNQcr0gJ7pOmP",
-          amount:1*100,
-          currency:"INR",
-          name:"Hyderabad Metro",
-          image:img2,
-          description:"For ticket booking transactions",
-          handler:function(response) {
-            setpay(true);
-          },
-          prefill :{
-            email:user.email
-          },
-          notes :{
-             address:"Hyderabad metro Office"
-          },
-          theme :{
-            color:"blue"
+        setflagc(true);
+        const tofrom=valTo+valFrom;
+        const fromto=valFrom+valTo;
+        const stationcostref=collection(db,'Cost');
+        getDocs(stationcostref).then(response => {
+          const k= response.docs.map(doc => ({
+            data:doc.data()
+          }))
+          console.log(k);
+          try {
+            for(var s in k) {
+            console.log(k[s].data);
+            if(k[s].data.FromTo===tofrom || k[s].data.FromTo===fromto) {
+              setAmt(k[s].data.cost)
+              console.log(k[s].data.cost)
+              break;
+            }
           }
-        };
-        //console.log(data);
-        var pay1=new window.Razorpay(options);
-        pay1.open();
-        setError("Payment Successfull"); 
+          // console.log(Amt);
+          // console.log(tofrom);
+        }catch(e) {
+
+        }
+        }).catch(error=>console.log(error.message));
       }
       console.log(pay);
+      //console.log(costv);
   }
   const generateQr=async(e)=>{
     setpay(false);
@@ -86,6 +141,7 @@ function Loggedin() {
       setpay(false);
   }
     const pdfGenerate=()=>{
+      setAmt("");
       setpay(false);
       console.log(pay);
         var date1 = moment();
@@ -118,6 +174,36 @@ function Loggedin() {
          setValFrom("");
          navigate("/Loggedin");
     }
+    const handlepayment=async(e)=>{
+      e.preventDefault();
+      if(Amt!=="") {
+        var options= {
+          key:"rzp_test_Rg7Lgz7NgVAUCu",
+          key_secret:"x0e5GSKDeyVrNQcr0gJ7pOmP",
+          amount:Amt*100,
+          currency:"INR",
+          name:"Hyderabad Metro",
+          image:img2,
+          description:"For ticket booking transactions",
+          handler:function(response) {
+            setpay(true);
+          },
+          prefill :{
+            email:user.email
+          },
+          notes :{
+             address:"Hyderabad metro Office"
+          },
+          theme :{
+            color:"blue"
+          }
+        };
+        //console.log(data);
+        var pay1=new window.Razorpay(options);
+        pay1.open();
+        setflagc(false);
+      }
+    }
     return (
       <>
       <div className="p-4 box mt-3 text-center text-success">
@@ -125,7 +211,7 @@ function Loggedin() {
           {user && user.email}
       </div>    
       <div className='pp1 box gap-4 mt-4 '>
-      {!img1 && !pay && (<div>  <h4 className='p-4 '>Book Metro QR Ticket</h4>
+      {!flagc && !img1 && !pay && (<div>  <h4 className='p-4 '>Book Metro QR Ticket</h4>
         {error==="Invalid Input" && <Alert variant="danger">{error}</Alert>}
         {/* {error!=="Invalid Input" && error!=="" && <Alert variant="success">{error}</Alert>} */}
          <div className='box p-3 d-flex'>
@@ -164,20 +250,51 @@ function Loggedin() {
          <option value="13">Gandhi Hospital</option>
        </select>
       </div>
-      {<div className='pt-3 pb-3 b1 d-flex'>
+      {!flagc && <div className='pt-3 pb-3 b1 d-flex'>
         <Button type='submit' onClick={handleSubmit}>Submit</Button>
-      </div>}  </div> )}
+      </div>}  
+      </div> )}
+      {
+        flagc && (
+          <div className='text-center'>
+            <div><h4 className='p-2'>Pay for Your Ticket from{<h4 className='text-primary'>{Stations[valFrom - 1]}</h4>}to{<h4 className='text-primary'>{Stations[valTo - 1]}</h4>}</h4></div>
+            {error!=="Invalid Input" && error!=="" && <Alert variant="success">{error}</Alert>} 
+            <div className='m-1 p-2'><Button variant="success" onClick={handlepayment}  type="submit" >Make Payment</Button></div>
+          </div>
+          //<a href={img1}   download= "qr.pdf"></a>
+        )
+      }
       { pay && <div className='text-center p-3'>
-      {error!=="Invalid Input" && error!=="" && <Alert variant="success">{error}</Alert>} 
+      {<Alert variant="success">Payment of rupees {<h4>{Amt}</h4>} Successfull</Alert>} 
         <Button type='submit' onClick={generateQr}>Generate Ticket</Button>
       </div>} 
       {
-        img1 && (
+        img1 && !sendflag && (
           <div className='text-center'>
             <div><h4 className='pt-3'>Download Your Ticket</h4></div>
             {error!=="Invalid Input" && error!=="" && <Alert variant="success">{error}</Alert>} 
             <div><img src={img1} alt="qr"></img></div>
-            <div className='p-3'><Button variant="success" onClick={pdfGenerate}  type="submit" >Download</Button></div>
+            <div className='m-1 p-1'><Button variant="success" onClick={pdfGenerate}  type="submit" >Download</Button></div>
+            <p>OR</p>
+            <div className='m-3'><Button variant="primary" >Booked For Others ?</Button></div>
+          </div>
+          //<a href={img1}   download= "qr.pdf"></a>
+        )
+      }
+      {
+        img1 && sendflag && (
+          <div className='text-center m-2'>
+            <div><h4 className='p-2 m-2'>Enter Phone Number</h4></div>
+            {error==="Invalid Input" && error!=="" && <Alert  variant="danger">{error}</Alert>} 
+            <div>
+            <PhoneInput
+              defaultCountry="IN"
+              //value={number}
+              color="blue"
+              onChange={setNumber}
+              placeholder="Enter Phone Number"
+            /></div>
+            <div className='m-2 p-2'><Button variant="success" onClick={sendpdf2}  >Send Ticket</Button></div>
           </div>
           //<a href={img1}   download= "qr.pdf"></a>
         )
